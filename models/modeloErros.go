5301815c505b7bd9f1a9aa16a7410e99b4deecc9
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-03-26 19:27:49 -0300

    Criando modelo pra erros

---
+++ models/modeloErros.go
@@ -0,0 +1,6 @@
+package models
+
+var ErrosSlice = map[string]string{
+	"ErroAberturaPostgres": "Erro ao tentar abrir o Postgres.",
+	"ErroConexaoPostgres":  "Erro ao tentar conectar ao Postgres.",
+}
