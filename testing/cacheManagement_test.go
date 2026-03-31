3c8d0f9a995cab10c9ce55177426ca0cbaa2e1d4
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-03-31 07:22:58 -0300

    Finalizando testes do valkey.

--- testing/cacheManagement_test.go
+++ testing/cacheManagement_test.go
@@ -4,16 +4,28 @@ import (
 	"testing"
 
 	"github.com/alvarolucio2007/uptime-sentinel-go-cli/cache"
+	"github.com/alvarolucio2007/uptime-sentinel-go-cli/models"
 )
 
 func TestValkey(t *testing.T) {
 	t.Run("Fluxo Completo Valkey", func(t *testing.T) {
-		// colocar função aqui...
+		testarValkey(t)
 	})
 }
 
 func testarValkey(t *testing.T) {
 	if err := cache.ConectarCache(); err != nil {
 		t.Errorf("Erro ao se concectar ao cache: %v", err)
+	}
+	ID, urlOriginal := "1000", &models.ModeloLink{ID: 1000, URL: "https://google.com", PeriodoSegundos: 2, StatusEsperado: 200}
+	if err := cache.AdicionarLinkValkey(ID, urlOriginal); err != nil {
+		t.Errorf("Erro ao adicionar link ao valkey: %v", err)
+	}
+	urlRecuperada, err := cache.BuscarLinkValkey("1000")
+	if err != nil {
+		t.Errorf("Erro ao buscar link no valkey: %v", err)
+	}
+	if urlRecuperada != urlOriginal.URL {
+		t.Errorf("Erro ao comparar URL recuperada e URL original, recebi %s e recebi %s", urlOriginal.URL, urlRecuperada)
 	}
 }
