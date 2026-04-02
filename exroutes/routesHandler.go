443d2f626a151ebaf21c5ff7bb210b0af2cdf225
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-02 19:02:29 -0300

    Ajustando tipo de variavel do input, no HandlerAdd, e continuando a fazer o handler

--- exroutes/routesHandler.go
+++ exroutes/routesHandler.go
@@ -5,12 +5,16 @@ import (
 	"net/http"
 
 	"github.com/alvarolucio2007/uptime-sentinel-go-cli/models"
+	"github.com/alvarolucio2007/uptime-sentinel-go-cli/verifiers"
 )
 
 func HandlerAdd(w http.ResponseWriter, r *http.Request) error {
-	var input models.ModeloLink
+	var input *models.ModeloLink
 	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
 		http.Error(w, models.ErroDecodeJSONAPIExterna.Mensagem, http.StatusBadRequest)
 		return err
+	}
+	if err := verifiers.ValidacaoCompleta(input); err != nil {
+		// Tenho que retornar bad request mas n lembro como é, vou fazer os testes da validação kk
 	}
 }
