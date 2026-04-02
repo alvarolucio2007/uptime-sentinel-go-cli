d8c6d7d7e945978cb98da40447380879dc6eb066
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-02 19:51:23 -0300

    Testes de verificadores feitos com sucesso, testes positivos e negativos.

--- test/verifiers_test.go
+++ test/verifiers_test.go
@@ -1,9 +1,32 @@
 package testing
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/alvarolucio2007/uptime-sentinel-go-cli/models"
+	"github.com/alvarolucio2007/uptime-sentinel-go-cli/verifiers"
+)
 
 func TestVerifiers(t *testing.T) {
 	t.Run("Fluxo completo de testes", func(t *testing.T) {
-		// Função...
+		testVerificarLink(t)
 	})
+}
+
+func testVerificarLink(t *testing.T) {
+	linksTeste := []struct {
+		nome       string
+		modeloLink models.ModeloLink
+		passa      bool
+	}{
+		{"Passa", models.ModeloLink{URL: "https://google.com", PeriodoSegundos: 2, StatusEsperado: 200}, true},
+		{"Não passa: sem http nem https", models.ModeloLink{URL: "google.com", PeriodoSegundos: 2, StatusEsperado: 200}, false},
+		{"Não passa: sem host nem scheme", models.ModeloLink{URL: "teste", PeriodoSegundos: 2, StatusEsperado: 200}, false},
+		{"Não passa: status inválido", models.ModeloLink{URL: "https://google.com", PeriodoSegundos: 2, StatusEsperado: 125}, false},
+	}
+	for _, testCase := range linksTeste {
+		if (verifiers.ValidacaoCompleta(&testCase.modeloLink) == nil) != testCase.passa {
+			t.Errorf("Erro: resultado de %s diferente do esperado %t", testCase.modeloLink.URL, testCase.passa)
+		}
+	}
 }
