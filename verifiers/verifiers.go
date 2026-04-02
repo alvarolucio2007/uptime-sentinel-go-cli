fb924db74cad98b71229d5b4d66dba02b6db60fc
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-02 17:43:14 -0300

    Começando arquivo de verificadores, já com verificador de URL, que retorna bool.

---
+++ verifiers/verifiers.go
@@ -0,0 +1,22 @@
+// Package verifiers verifica os links, periodo em segundos e status esperado.
+package verifiers
+
+import (
+	"net/url"
+	"strings"
+)
+
+func ValidarURL(URL string) bool {
+	u, err := url.Parse(URL)
+	if err != nil {
+		return false
+	}
+	if u.Scheme == "" || u.Host == "" {
+		return false
+	}
+	scheme := strings.ToLower(u.Scheme)
+	return scheme == "http" || scheme == "https"
+}
+
+func ValidarStatus(status uint) bool {
+}
