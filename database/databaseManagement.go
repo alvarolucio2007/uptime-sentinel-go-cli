f419f686e24dbc234117bcd4641990700459adf8
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-06 10:16:15 -0300

    Adicionando função antiga para refatoração, e refatorando função de adicionar.

--- database/databaseManagement.go
+++ database/databaseManagement.go
@@ -30,16 +30,24 @@ func ConectarDatabase() error {
 	return nil
 }
 
-func checarResultadoPostgres(resultado *gorm.DB) error {
-	return resultado.Error
+func MigrarBanco() error {
+	query := `CREATE TABLE IF NOT EXISTS linksSentinel (
+		ID SERIAL PRIMARY KEY,
+		URL TEXT UNIQUE NOT NULL,
+		PeriodoSegundos INTEGER NOT NULL DEFAULT 0,
+		StatusEsperado INTEGER NOT NULL
+	);`
+	if _, err := DB.Exec(query); err != nil {
+		return err
+	}
+	fmt.Println("Tabela pronta para uso.")
+	return nil
 }
 
-func CriarEntradaPostgres(URL string, periodo, statusEsperado uint) error {
-	var id uint
-	query := "INSERT INTO linksSentinel (URL,PeriodoSegundos,StatusEsperado) VALUES ($1,$2,$3) RETURNING id"
-	if err := DB.QueryRow(query, URL, periodo, statusEsperado).Scan(&id); err != nil {
-		models.ErroEntradaPostgres.Log(err)
-		return err
+func CriarEntradaPostgres(dados models.ModeloLink) error {
+	if resultado := DB.Create(&dados); resultado.Error != nil {
+		models.ErroEntradaPostgres.Log(resultado.Error)
+		return resultado.Error
 	}
 	return nil
 }
