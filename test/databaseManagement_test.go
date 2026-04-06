cff7e2ae947f0780fd21a4ad227adf46fc2c5693
Álvaro Lúcio <alvarolucio2007@proton.me>
2026-04-06 15:04:34 -0300

    Reescrevendo teste de database

--- test/databaseManagement_test.go
+++ test/databaseManagement_test.go
@@ -15,22 +15,21 @@ func TestPostgres(t *testing.T) {
 
 func testarPostgres(t *testing.T) {
 	if err := database.ConectarDatabase(); err != nil {
-		t.Errorf("Erro ao se conectar à base de dados: %v", err)
+		t.Fatalf("Erro ao se conectar à base de dados: %v", err)
 	}
-	query := "DROP TABLE IF EXISTS linksSentinel"
-	if _, err := database.DB.Exec(query); err != nil {
-		t.Errorf("Erro ao limpar a base de dados: %v", err)
+	if err := database.DB.Migrator().DropTable(&models.ModeloLink{}); err != nil {
+		t.Fatalf("Erro ao dar drop table: %v", err)
 	}
-	if err := database.MigrarBanco(); err != nil {
-		t.Errorf("Erro ao migrar a base de dados: %v", err)
+	if err := database.DB.AutoMigrate(&models.ModeloLink{}); err != nil {
+		t.Errorf("Erro ao migrar database: %v", err)
 	}
 	entradaDB := [2]models.ModeloLink{
 		{URL: "https://google.com", PeriodoSegundos: 10, StatusEsperado: uint(101)}, // StatusEsperado só placeholder msm kk
 		{URL: "https://github.com", PeriodoSegundos: 10, StatusEsperado: uint(101)},
 	}
-	for _, entrada := range entradaDB {
-		if err := database.CriarEntradaPostgres(entrada.URL, entrada.PeriodoSegundos, entrada.StatusEsperado); err != nil {
-			t.Errorf("Erro ao adicionar livro: %v", err)
+	for _, e := range entradaDB {
+		if err := database.CriarEntradaPostgres(e); err != nil {
+			t.Errorf("Falha ao inserir %s:%v", e.URL, err)
 		}
 	}
 }
